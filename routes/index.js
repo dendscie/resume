885fd62a1d67e72840a432f3f8e593f3e844804d
Kevin Vasseure <kevin.vasseure@gmail.com>
2015-03-08 15:58:43 +0900

    Dynamic Content  with json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var resumeData = JSON.parse(fs.readFileSync("./data/en/resume.json"));
+    res.render('index',  {"resume" : resumeData});
 });
 
 module.exports = router;
